6f2e825fcd07a8949f0928eec1a98e23840ef66e
Brian Woodward <brian.woodward@gmail.com>
2015-12-15 16:13:23 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * grid-prompt <https://github.com/doowb/grid-prompt>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
